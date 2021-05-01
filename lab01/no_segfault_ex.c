e994ab2053a3eb9a3b37bc6bc24f9338a8a26fcd
RshStone <1229077499@qq.com>
2021-05-01 09:08:36 +0800

    5.1

--- lab01/no_segfault_ex.c
+++ lab01/no_segfault_ex.c
@@ -2,8 +2,10 @@
 int main() {
     int a[5] = {1, 2, 3, 4, 5};
     unsigned total = 0;
-    for (int j = 0; j < sizeof(a); j++) {
+	printf("size of a: %d\n", sizeof(a));
+    for (int j = 0; j < sizeof(a)/sizeof(int); j++) {
         total += a[j];
+        printf("%d\n",total);
     }
     printf("sum of array is %d\n", total);
 }
